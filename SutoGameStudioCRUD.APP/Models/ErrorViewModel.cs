545df8ed5fb6c795966e56ff11d56d57194222cb
Elijah Smith <elijah.d.s@outlook.com>
2023-09-11 12:54:32 -0400

    Add project files.

---
+++ SutoGameStudioCRUD.APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SutoGameStudioCRUD.APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
