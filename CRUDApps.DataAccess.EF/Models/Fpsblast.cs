8de71f2a77c71584838a038f06cb4426f73819d0
Elijah Smith <elijah.d.s@outlook.com>
2023-09-20 21:00:43 -0400

    Adding the basic model for the Fpsblast class

---
+++ CRUDApps.DataAccess.EF/Models/Fpsblast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDApps.DataAccess.EF.Models
+{
+    public partial class Fpsblast
+    {
+        public Fpsblast()
+        {
+            LoyaltyCharts = new HashSet<LoyaltyChart>();
+        }
+
+        public int FpsblastId { get; set; }
+        public string UserName { get; set; } = null!;
+        public TimeSpan TimePlayed { get; set; }
+        public string Expansion1 { get; set; } = null!;
+        public string OwnFpsblastGame { get; set; } = null!;
+
+        public virtual ICollection<LoyaltyChart> LoyaltyCharts { get; set; }
+    }
+}
