1f4f49d8520166a9e0b02423a0cb0502d9db1f89
Elijah Smith <elijah.d.s@outlook.com>
2023-09-20 21:01:49 -0400

    Adding the basic model for the Mmoslasher class

---
+++ CRUDApps.DataAccess.EF/Models/Mmoslasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDApps.DataAccess.EF.Models
+{
+    public partial class Mmoslasher
+    {
+        public Mmoslasher()
+        {
+            LoyaltyCharts = new HashSet<LoyaltyChart>();
+        }
+
+        public int MmoslasherId { get; set; }
+        public string UserName { get; set; } = null!;
+        public TimeSpan TimePlayed { get; set; }
+        public string Expansion1 { get; set; } = null!;
+        public string ActiveLast30Days { get; set; } = null!;
+        public string OwnMmoslasherGame { get; set; } = null!;
+
+        public virtual ICollection<LoyaltyChart> LoyaltyCharts { get; set; }
+    }
+}
