59b9bbf9b1fced31485e90d908bb3a04b3624199
Elijah Smith <elijah.d.s@outlook.com>
2023-09-20 21:02:18 -0400

    Adding the basic model for the User class

---
+++ CRUDApps.DataAccess.EF/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDApps.DataAccess.EF.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string CustomerFirstName { get; set; } = null!;
+        public string CustomerLastName { get; set; } = null!;
+        public string CustomerEmail { get; set; } = null!;
+        public string CustomerState { get; set; } = null!;
+
+        public virtual LoyaltyChart UserNameNavigation { get; set; } = null!;
+    }
+}
