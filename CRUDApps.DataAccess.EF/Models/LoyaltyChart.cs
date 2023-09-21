0c5d3e93acb62c5d969eb015dc0b395e5a0301f9
Elijah Smith <elijah.d.s@outlook.com>
2023-09-20 21:01:12 -0400

    Adding the basic model for the LoyaltyChart class

---
+++ CRUDApps.DataAccess.EF/Models/LoyaltyChart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDApps.DataAccess.EF.Models
+{
+    public partial class LoyaltyChart
+    {
+        public LoyaltyChart()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public int LoyaltyChartId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string OwnFpsblastGame { get; set; } = null!;
+        public string OwnMmoslasherGame { get; set; } = null!;
+        public string OwnRpgstoryMakerGame { get; set; } = null!;
+        public string IsLoyalCustomer { get; set; } = null!;
+
+        public virtual Fpsblast OwnFpsblastGameNavigation { get; set; } = null!;
+        public virtual Mmoslasher OwnMmoslasherGameNavigation { get; set; } = null!;
+        public virtual RpgstoryMaker OwnRpgstoryMakerGameNavigation { get; set; } = null!;
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
