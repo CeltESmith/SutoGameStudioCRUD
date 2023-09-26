58a8e3f0a099d6a784a2afa44fe2355cdc55671b
Elijah Smith <elijah.d.s@outlook.com>
2023-09-25 22:24:24 -0400

    creation of the Models folder and the RpgstoryMaker class base file.

---
+++ CRUDApps.DataAccess.EF/Models/RpgstoryMaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDApps.DataAccess.EF.Models
+{
+    public partial class RpgstoryMaker
+    {
+        public RpgstoryMaker()
+        {
+            LoyaltyCharts = new HashSet<LoyaltyChart>();
+        }
+
+        public int RpgstoryMakerId { get; set; }
+        public string UserName { get; set; } = null!;
+        public TimeSpan TimePlayed { get; set; }
+        public string GameCompleted { get; set; } = null!;
+        public string OwnRpgstoryMakerGame { get; set; } = null!;
+
+        public virtual ICollection<LoyaltyChart> LoyaltyCharts { get; set; }
+    }
+}
