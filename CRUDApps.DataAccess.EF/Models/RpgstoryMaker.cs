97f75df83430eaa5d4df8a2481715cb73cd026c5
Elijah Smith <elijah.d.s@outlook.com>
2023-09-28 16:34:43 -0400

    Updated the RpgstoryMaker Model

--- CRUDApps.DataAccess.EF/Models/RpgstoryMaker.cs
+++ CRUDApps.DataAccess.EF/Models/RpgstoryMaker.cs
@@ -5,10 +5,6 @@ namespace CRUDApps.DataAccess.EF.Models
 {
     public partial class RpgstoryMaker
     {
-        public RpgstoryMaker()
-        {
-            LoyaltyCharts = new HashSet<LoyaltyChart>();
-        }
 
         public int RpgstoryMakerId { get; set; }
         public string UserName { get; set; } = null!;
@@ -17,5 +13,20 @@ namespace CRUDApps.DataAccess.EF.Models
         public string OwnRpgstoryMakerGame { get; set; } = null!;
 
         public virtual ICollection<LoyaltyChart> LoyaltyCharts { get; set; }
+
+        public RpgstoryMaker (int rpgstoryMakerId, string userName, TimeSpan timePlayed, string gameCompleted, string ownRpgstoryMakerGame, ICollection<LoyaltyChart> loyaltyChart)
+        {
+            RpgstoryMakerId = rpgstoryMakerId;
+            UserName = userName;
+            TimePlayed = timePlayed;
+            GameCompleted = gameCompleted;
+            OwnRpgstoryMakerGame = ownRpgstoryMakerGame;
+            LoyaltyCharts = new HashSet<LoyaltyChart>(loyaltyChart);
+        }
+
+        public RpgstoryMaker()
+        {
+            LoyaltyCharts = new HashSet<LoyaltyChart>();
+        }
     }
 }
