9a19133dbd63462593d5186cbdf7a0e07a1f2ca3
Elijah Smith <elijah.d.s@outlook.com>
2023-09-28 16:33:49 -0400

    Updated the MmoSlasher Model

--- CRUDApps.DataAccess.EF/Models/Mmoslasher.cs
+++ CRUDApps.DataAccess.EF/Models/Mmoslasher.cs
@@ -5,10 +5,6 @@ namespace CRUDApps.DataAccess.EF.Models
 {
     public partial class Mmoslasher
     {
-        public Mmoslasher()
-        {
-            LoyaltyCharts = new HashSet<LoyaltyChart>();
-        }
 
         public int MmoslasherId { get; set; }
         public string UserName { get; set; } = null!;
@@ -18,5 +14,21 @@ namespace CRUDApps.DataAccess.EF.Models
         public string OwnMmoslasherGame { get; set; } = null!;
 
         public virtual ICollection<LoyaltyChart> LoyaltyCharts { get; set; }
+
+        public Mmoslasher(int mmoslasherId, string userName, TimeSpan timePlayed, string expansion1, string activeLast30Days, string ownMmoslasherGame, ICollection<LoyaltyChart> loyaltyChart)
+        {
+            MmoslasherId = mmoslasherId;
+            UserName = userName;
+            TimePlayed = timePlayed;
+            Expansion1 = expansion1;
+            ActiveLast30Days = activeLast30Days;
+            OwnMmoslasherGame = ownMmoslasherGame;
+            LoyaltyCharts = new HashSet<LoyaltyChart>(loyaltyChart);
+        }
+
+        public Mmoslasher()
+        {
+            LoyaltyCharts = new HashSet<LoyaltyChart>();
+        }
     }
 }
